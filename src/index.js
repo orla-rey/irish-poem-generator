879bfcc38de0244669ccce6971da5569253aff2a
orla-rey <orla.o.reynolds@gmail.com>
2025-03-21 10:05:09 -0400

    improved user experience

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ function displayPoem(response) {
      autoStart: true,
      delay: 1,
      cursor: "",
-     pauseFor: 100000,
    });
 }
 
@@ -20,6 +19,10 @@ function generatePoem(event) {
   let context = "You are an Irish poem expert and love to write short poems. Your mission is to generate a short Irish Poem in basic HTML and separate each line with <br />. Make sure to follow the user instructions. Please do not generating anything mentioning HTML, only generate the poem. The poem doe not need to be in quotes at all. Please always include a title inside <strong> element.";
   let apiUrl =
     `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class = "generating">⏳ Generating an Irish poem for you about ${promptInput.value}</div>`
 
   console.log("Generating Poem");  
   console.log(`Prompt: ${prompt}`);
